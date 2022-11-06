393134972dd4559dc9161e743fffb191356d157f
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-06 20:13:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ client.on('ready', () => {
 
  
 client.on('message',async message => {
-	if(message.body === 'hii') {
-		client.sendMessage(message.from, await MessageMedia.fromUrl('https://admin.nadith.pro/www.1TamilMV.vin%20-%20PAAGAL%20(2021)%20Telugu%20_%20-%20720p%20-%20HEVC%20-%20(DD5.1%20-%20192Kbps%20%20AAC%202.0)%20-%20950MB%20-%20ESub.mkv'));
+	if(message.body === 'movie') {
+		client.sendMessage(message.from, await MessageMedia.fromUrl('https://cloud.nadith.pro/en_mv/How_To_Train_Your_Dragon_2010_@nadithpro.mkv'));
 	}
 });
 
